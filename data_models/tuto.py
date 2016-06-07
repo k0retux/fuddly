bfa8b79ca426229c573e4bb6761b0e7c067124e7
Julien Baladier <julien.baladier@gmail.com>
2016-06-07 17:50:34 +0200

    Add a more complex example of BitfieldCondition usage in the doc

--- data_models/tuto.py
+++ data_models/tuto.py
@@ -228,7 +228,7 @@ class MyDF_DataModel(DataModel):
 
              {'name': 'A32_payload',
               'contents': String(val_list=['$ A32_VALID $', '$ A32_INVALID $'], determinist=False),
-              'exists_if': (BitFieldCondition(sf=2, val=5), 'A3_subopcode')}
+              'exists_if': (BitFieldCondition(sf=[0, 1, 2], val=[[500, 501], [1, 2], 5]), 'A3_subopcode')}
          ]}
 
 
