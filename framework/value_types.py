k0retux/fuddly

commit 5e92e7ea6c6476a498b92e7acf3f46b1d8dcf09b

Fix String absorption regarding encoding. (TBC)

framework/value_types.py
@@ -442,21 +442,23 @@ class String(VT_Alt):
 
         elif constraints[AbsCsts.Contents] and self.alphabet is not None:
             size = None
-            blob = unconvert_from_internal_repr(blob)
             if self.encoded_string:
-                blob_dec = unconvert_from_internal_repr(blob_dec)
+                blob_str = unconvert_from_internal_repr(blob_dec)
             else:
+                blob_str = unconvert_from_internal_repr(blob)
                 blob_dec = blob
             alp = unconvert_from_internal_repr(self.alphabet)
             for l in alp:
-                if blob_dec.startswith(l):
+                if blob_str.startswith(l):
                     break
             else:
                 sup_sz = len(blob)+1
                 off = sup_sz
                 for l in alp:
                     if self.encoded_string:
-                        l = self.encode(l)
+                        l = self.encode(convert_to_internal_repr(l))
+                    else:
+                        l = convert_to_internal_repr(l)
                     new_off = blob.find(l)
                     if new_off < off and new_off > -1:
                         off = new_off
@@ -464,15 +466,16 @@ class String(VT_Alt):
                     off = -1
 
         elif constraints[AbsCsts.Regexp] and self.regexp is not None:
-            g = re.search(self.regexp, blob_dec, re.S)
+            g = re.search(self.regexp, unconvert_from_internal_repr(blob_dec), re.S)
             if g is not None:
                 if self.encoded_string:
                     pattern_enc = self.encode(g.group(0))
                     off = blob.find(pattern_enc)
                     size = len(pattern_enc)
                 else:
-                    off = g.start()
-                    size = g.end() - off
+                    pattern_enc = convert_to_internal_repr(g.group(0))
+                    off = blob.find(pattern_enc)
+                    size = len(pattern_enc)
             else:
                 off = -1
 
@@ -505,10 +508,7 @@ class String(VT_Alt):
         self.orig_drawn_val = self.drawn_val
 
         if constraints[AbsCsts.Size]:
-            if self.encoded_string:
-                sz = size if size is not None and size < self.max_encoded_sz else self.max_encoded_sz
-            else:
-                sz = size if size is not None and size < self.max_sz else self.max_sz
+            sz = size if size is not None and size < self.max_encoded_sz else self.max_encoded_sz
 
             # if encoded string, val is returned decoded
             val = self._read_value_from(blob[off:sz+off], constraints)
@@ -519,8 +519,8 @@ class String(VT_Alt):
                     raise ValueError('min_encoded_sz constraint not respected!')
             else:
                 val_sz = len(val) # maybe different from sz if blob is smaller
-                if val_sz < self.min_sz:
-                    raise ValueError('min_sz constraint not respected!')
+                if val_sz < self.min_encoded_sz:
+                    raise ValueError('min_sz/min_encoded_sz constraint not respected!')
         else:
             blob = blob[off:] #blob[off:size+off] if size is not None else blob[off:]
             val = self._read_value_from(blob, constraints)
@@ -653,10 +653,10 @@ class String(VT_Alt):
 
         self.drawn_val = None
 
-    def _check_sizes(self):
-        if self.val_list is not None:
-            for v in self.val_list:
-                sz = len(unconvert_from_internal_repr(v))
+    def _check_sizes(self, val_list):
+        if val_list is not None:
+            for v in val_list:
+                sz = len(v)
                 if self.max_sz is not None:
                     assert(self.max_sz >= sz >= self.min_sz)
                 else:
@@ -682,9 +682,9 @@ class String(VT_Alt):
 
         if absorb_regexp is None:
             if self.ascii_mode:
-                self.regexp = b'[\x00-\x7f]*'
+                self.regexp = '[\x00-\x7f]*'
             else:
-                self.regexp = b'.*'
+                self.regexp = '.*'
         else:
             self.regexp = convert_to_internal_repr(absorb_regexp)
 
@@ -696,7 +696,7 @@ class String(VT_Alt):
             self.extra_fuzzy_list = None
 
         if val_list is not None:
-            assert(isinstance(val_list, list))
+            assert isinstance(val_list, list)
             self.val_list = VT._str2internal(val_list)
             for val in self.val_list:
                 if not self._check_compliance(val, force_max_enc_sz=max_encoded_sz is not None,
@@ -708,6 +708,18 @@ class String(VT_Alt):
                     for l in val:
                         if l not in self.alphabet:
                             raise ValueError("The value '%s' does not conform to the alphabet!" % val)
+
+            if isinstance(self, String):
+                max_enc_sz = 0
+                min_enc_sz = len(self.val_list[0])
+                for val in self.val_list:
+                    length = len(val)
+                    if length > max_enc_sz:
+                        max_enc_sz = length
+                    if length < min_enc_sz:
+                        min_enc_sz = length
+                self.max_encoded_sz = max_enc_sz
+                self.min_encoded_sz = min_enc_sz
 
             self.val_list_copy = copy.copy(self.val_list)
             self.is_val_list_provided = True  # distinguish cases where
@@ -721,45 +733,37 @@ class String(VT_Alt):
         if size is not None:
             self.min_sz = size
             self.max_sz = size
-
         elif min_sz is not None and max_sz is not None:
             assert(max_sz >= 0 and min_sz >= 0 and max_sz - min_sz >= 0)
             self.min_sz = min_sz
             self.max_sz = max_sz
-
         elif min_sz is not None:
             self.min_sz = min_sz
             # for string with no size limit, we set a threshold to
             # DEFAULT_MAX_SZ chars
             self.max_sz = self.DEFAULT_MAX_SZ
-            
         elif max_sz is not None:
             self.max_sz = max_sz
             self.min_sz = 0
-
         elif val_list is not None:
             sz = 0
-            for v in self.val_list:
+            for v in val_list:
                 length = len(v)
                 if length > sz:
                     sz = length
-
             self.max_sz = sz
             self.min_sz = 0
-
         elif max_encoded_sz is not None:
             # If we reach this condition, that means no size has been provided, we thus decide
             # an arbitrary default value for max_sz. Regarding absorption, this arbitrary choice will
             # have no influence, as only max_encoded_sz will be used.
             self.min_sz = 0
             self.max_sz = max_encoded_sz
-
         else:
             self.min_sz = 0
             self.max_sz = self.DEFAULT_MAX_SZ
 
-
-        self._check_sizes()
+        self._check_sizes(val_list)
 
         if val_list is None:
             self._populate_val_list(force_max_enc_sz=max_encoded_sz is not None,

test/integration/test_integration.py
@@ -2195,7 +2195,7 @@ class TestNodeFeatures(unittest.TestCase):
         gen_exist_desc = \
             {'name': 'gen_exist_cond',
              'separator': {'contents': {'name': 'sep_nl',
-                                        'contents': String(val_list=['\n'], max_sz=100, absorb_regexp=b'[\r\n|\n]+'),
+                                        'contents': String(val_list=['\n'], max_sz=100, absorb_regexp='[\r\n|\n]+'),
                                         'absorb_csts': AbsNoCsts(regexp=True)},
                            'prefix': False, 'suffix': False, 'unique': True},
              'contents': [
@@ -2465,7 +2465,7 @@ class TestNode_NonTerm(unittest.TestCase):
              'determinist': True,
              'separator': {'contents': {'name': 'SEP',
                                         'contents': String(val_list=[' ', '  ', '     '],
-                                                           absorb_regexp=b'\s+', determinist=False),
+                                                           absorb_regexp='\s+', determinist=False),
                                         'absorb_csts': AbsNoCsts(regexp=True)},
                            'prefix': True,
                            'suffix': True,

data_models/example.py
@@ -357,7 +357,7 @@ class Example_DataModel(DataModel):
                     'custo_set': MH.Custo.NTerm.FrozenCopy,
                     'custo_clear': MH.Custo.NTerm.MutableClone,
                     'separator': {'contents': {'name': 'sep',
-                                               'contents': String(val_list=['\n'], absorb_regexp=b'\n+'),
+                                               'contents': String(val_list=['\n'], absorb_regexp='\n+'),
                                                'absorb_csts': AbsNoCsts(regexp=True)}},
                     'contents': [{
                         'section_type': MH.Random,

data_models/tuto.py
@@ -41,7 +41,7 @@ class MyDF_DataModel(DataModel):
                    'custo_set': MH.Custo.NTerm.FrozenCopy,
                    'custo_clear': MH.Custo.NTerm.MutableClone,
                    'separator': {'contents': {'name': 'sep',
-                                              'contents': String(val_list=['\n'], absorb_regexp=b'\n+'),
+                                              'contents': String(val_list=['\n'], absorb_regexp='\n+'),
                                               'absorb_csts': AbsNoCsts(regexp=True)}},
                    'contents': [{
                        'section_type': MH.Random,
@@ -150,7 +150,7 @@ class MyDF_DataModel(DataModel):
         separator_desc = \
         {'name': 'separator',
          'separator': {'contents': {'name': 'sep_nl',
-                                    'contents': String(val_list=['\n'], absorb_regexp=b'[\r\n|\n]+'),
+                                    'contents': String(val_list=['\n'], absorb_regexp='[\r\n|\n]+'),
                                     'absorb_csts': AbsNoCsts(regexp=True)},
                        'prefix': False,
                        'suffix': False,
@@ -160,7 +160,7 @@ class MyDF_DataModel(DataModel):
               'contents': [
                   {'name': 'parameters',
                    'separator': {'contents': {'name': ('sep',2),
-                                              'contents': String(val_list=[' '], absorb_regexp=b' +'),
+                                              'contents': String(val_list=[' '], absorb_regexp=' +'),
                                               'absorb_csts': AbsNoCsts(regexp=True)}},
                    'qty': 3,
                    'contents': [

commit 2f09013084a2daf1edc99cafa0453a12589fc309

Fix String issues introduced by commit 5e92e7ea6c6476a498b92e7acf3f46b1d8dcf09b

framework/value_types.py
@@ -356,7 +356,7 @@ class String(VT_Alt):
             extra_fuzzy_list: During data generation, if this parameter is specified with some
               specific values, they will be part of the test cases generated by
               the generic disruptor tTYPE.
-            absorb_regexp: You can specify a regular expression in this parameter as a
+            absorb_regexp (str): You can specify a regular expression in this parameter as a
               supplementary constraint for data absorption operation.
             alphabet: The alphabet to use for generating data, in case no `val_list` is
               provided. Also use during absorption to validate the contents. It is
@@ -418,7 +418,6 @@ class String(VT_Alt):
     def absorb_auto_helper(self, blob, constraints):
         off = 0
         size = self.max_encoded_sz
-
         # If 'Contents' constraint is set, we seek for string within
         # val_list or conforming to the alphabet.
         # If 'Regexp' constraint is set, we seek for string matching
@@ -469,7 +468,7 @@ class String(VT_Alt):
             g = re.search(self.regexp, unconvert_from_internal_repr(blob_dec), re.S)
             if g is not None:
                 if self.encoded_string:
-                    pattern_enc = self.encode(g.group(0))
+                    pattern_enc = self.encode(convert_to_internal_repr(g.group(0)))
                     off = blob.find(pattern_enc)
                     size = len(pattern_enc)
                 else:
@@ -498,7 +497,6 @@ class String(VT_Alt):
         Returns:
             value, off, size
         """
-
         self.orig_max_sz = self.max_sz
         self.orig_min_encoded_sz = self.min_encoded_sz
         self.orig_max_encoded_sz = self.max_encoded_sz
@@ -628,11 +626,11 @@ class String(VT_Alt):
         if self.encoded_string:
             blob = self.decode(blob)
         if constraints[AbsCsts.Regexp]:
-            g = re.match(self.regexp, blob, re.S)
+            g = re.match(self.regexp, unconvert_from_internal_repr(blob), re.S)
             if g is None:
                 raise ValueError('regexp not valid!')
             else:
-                return g.group(0)
+                return convert_to_internal_repr(g.group(0))
         else:
             return blob
 
@@ -686,7 +684,7 @@ class String(VT_Alt):
             else:
                 self.regexp = '.*'
         else:
-            self.regexp = convert_to_internal_repr(absorb_regexp)
+            self.regexp = absorb_regexp
 
         if extra_fuzzy_list is not None:
             self.extra_fuzzy_list = VT._str2internal(extra_fuzzy_list)
@@ -708,18 +706,6 @@ class String(VT_Alt):
                     for l in val:
                         if l not in self.alphabet:
                             raise ValueError("The value '%s' does not conform to the alphabet!" % val)
-
-            if isinstance(self, String):
-                max_enc_sz = 0
-                min_enc_sz = len(self.val_list[0])
-                for val in self.val_list:
-                    length = len(val)
-                    if length > max_enc_sz:
-                        max_enc_sz = length
-                    if length < min_enc_sz:
-                        min_enc_sz = length
-                self.max_encoded_sz = max_enc_sz
-                self.min_encoded_sz = min_enc_sz
 
             self.val_list_copy = copy.copy(self.val_list)
             self.is_val_list_provided = True  # distinguish cases where
@@ -762,6 +748,14 @@ class String(VT_Alt):
         else:
             self.min_sz = 0
             self.max_sz = self.DEFAULT_MAX_SZ
+
+        if not self.encoded_string:
+            # In the case of String (or every non-Encoding type), the internal represenation
+            # is UTF8 encoding. Hence the byte string size is still >= to the string size.
+            if self.max_encoded_sz is None or self.max_encoded_sz < self.max_sz:
+                self.max_encoded_sz = self.max_sz
+            if self.min_encoded_sz is None or self.min_encoded_sz < self.min_sz:
+                self.min_encoded_sz = self.min_sz
 
         self._check_sizes(val_list)
 
@@ -813,6 +807,11 @@ class String(VT_Alt):
                 else:
                     return False
         else:
+            val_sz = len(value)
+            if self.max_encoded_sz is None or val_sz > self.max_encoded_sz:
+                self.max_encoded_sz = val_sz
+            if self.min_encoded_sz is None or val_sz < self.min_encoded_sz:
+                self.min_encoded_sz = val_sz
             if update_list:
                 self.val_list.append(value)
             return True

test/integration/test_integration.py
@@ -1847,7 +1847,7 @@ class TestNodeFeatures(unittest.TestCase):
 
         print('\n ---[message to absorb]---')
         print(repr(msg))
-        print('\n ---[absobed message]---')
+        print('\n ---[absorbed message]---')
         print(top.get_value())
 
         top.show()
@@ -2669,6 +2669,7 @@ class TestNode_TypedValue(unittest.TestCase):
                 return val + b'***'
 
             def decode(self, val):
+                print('\nDBg: ', val)
                 return val[:-3]
 
         data = ['Test!', 'Hello World!']