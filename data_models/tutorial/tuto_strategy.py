k0retux/fuddly

commit 3df7fe210507ff6a2f6a67a22f5dee4a980e2087

Fix regression with commit f1679374f2e7be24ddc93cbc1aedc44465f588e6
This new fix regarding the handling of separators should avoid incorrect behaviors
in some situation (using 'always' and 'suffix' properties).

framework/node.py
@@ -659,6 +659,9 @@ class NodeInternals(object):
 
     default_custo = None
 
+    def __hash__(self):
+        return id(self)
+
     def __init__(self, arg=None):
         # if new attributes are added, set_contents_from() have to be updated
         self.private = None
@@ -3531,8 +3534,9 @@ class NodeInternals_NonTerm(NodeInternals):
         for e in self.subnodes_set:
             e.tmp_ref_count = 1
 
-        if self.separator is not None and self.frozen_node_list and self.frozen_node_list[-1].is_attr_set(NodeInternals.AutoSeparator):
-            if not self.separator.suffix:
+        if self.separator is not None:
+            if not self.separator.suffix and self.frozen_node_list \
+                    and self.frozen_node_list[-1].is_attr_set(NodeInternals.AutoSeparator):
                 self.frozen_node_list.pop(-1)
             self._clone_separator_cleanup()
 
@@ -3618,7 +3622,9 @@ class NodeInternals_NonTerm(NodeInternals):
         node_list, was_not_frozen = self.get_subnodes_with_csts()
 
         djob_group_created = False
-        for n in node_list:
+        disabled_node = False
+        removed_cpt = 0
+        for idx, n in enumerate(node_list):
             if n.is_attr_set(NodeInternals.DISABLED):
                 val = Node.DEFAULT_DISABLED_NODEINT
                 if not n.env.is_djob_registered(key=id(n), prio=Node.DJOBS_PRIO_nterm_existence):
@@ -3631,12 +3637,30 @@ class NodeInternals_NonTerm(NodeInternals):
                                         cleanup = NodeInternals_NonTerm._cleanup_delayed_nodes,
                                         args=[n, node_list, len(l), conf, recursive],
                                         prio=Node.DJOBS_PRIO_nterm_existence)
+                    disabled_node = True
             else:
                 val = n._get_value(conf=conf, recursive=recursive,
                                    return_node_internals=True, enable_color=enable_color)
 
+            if self.separator is not None and isinstance(val, NodeInternals) and val.is_attr_set(NodeInternals.AutoSeparator) \
+                    and disabled_node and not self.separator.suffix and not self.separator.always:
+
+                # TODO: The case "suffix False and always True" should be already handled at
+                #  self.get_subnodes_with_csts()
+                #  --> TBC
+                disabled_node = False
+                self.frozen_node_list.pop(idx-removed_cpt)
+                removed_cpt += 1
+                continue
+
             # 'val' is always a NodeInternals except if non-term encoding has been carried out
             l.append(val)
+
+        if self.separator is not None and l and isinstance(l[-1], NodeInternals) \
+                and l[-1].is_attr_set(NodeInternals.AutoSeparator):
+            if not self.separator.suffix and not self.separator.always:
+                l.pop(-1)
+                self.frozen_node_list.pop(-1)
 
         if node_list:
             node_env = node_list[0].env
@@ -3676,13 +3700,6 @@ class NodeInternals_NonTerm(NodeInternals):
                                     for func, args, cleanup in djobs.values():
                                         if args[2] > job_idx:
                                             args[2] += node_qty-1
-
-        if self.separator is not None and l and isinstance(l[-1], NodeInternals) and \
-                l[-1].is_attr_set(NodeInternals.AutoSeparator):
-            if not self.separator.suffix:
-                l.pop(-1)
-                self.frozen_node_list.pop(-1)
-            self._clone_separator_cleanup()
 
         return (handle_encoding(l), was_not_frozen)
 

commit 6921855ddeaeea75cdf6c631b2f19d45d96cf415

Fix a bug introduced by commit 3df7fe210507ff6a2f6a67a22f5dee4a980e2087
The handling of nodes with the attribute NodeInternals.DISABLED (node.py) was buggy
since 3df7fe210507ff6a2f6a67a22f5dee4a980e2087, because the introduced
variable 'disabled_node' was not reset correctly in some situation,
making possible a wrong execution flow (reaching 'continue' on l.3673 in
an incorrect context).

data_models/tutorial/tuto_strategy.py
@@ -56,7 +56,7 @@ step1 = Step('exist_cond', fbk_timeout=1, set_periodic=[periodic1, periodic2],
              do_before_sending=before_sending_cbk, vtg_ids=0)
 step2 = Step('separator', fbk_timeout=2, clear_periodic=[periodic1], vtg_ids=1)
 empty = NoDataStep(clear_periodic=[periodic2])
-step4 = Step('off_gen', fbk_timeout=0)
+step4 = Step('off_gen', fbk_timeout=0, refresh_atoms=False)
 
 step1_copy = copy.copy(step1) # for scenario 2
 step2_copy = copy.copy(step2) # for scenario 2

framework/node.py
@@ -3672,6 +3672,8 @@ class NodeInternals_NonTerm(NodeInternals):
                 removed_cpt += 1
                 continue
 
+            disabled_node = False
+
             # 'val' is always a NodeInternals except if non-term encoding has been carried out
             l.append(val)
 

libs/debug_facility.py
@@ -21,11 +21,11 @@
 #
 ################################################################################
 
-DEBUG = True
+DEBUG = False
 LEVEL = 0
 
 # related to data_model.py
-DM_DEBUG = True
+DM_DEBUG = False
 ABS_DEBUG = False
 
 # related to value_types.py

test/integration/test_integration.py
@@ -4166,7 +4166,7 @@ class TestDataModelHelpers(unittest.TestCase):
             xml_atom.show()
             assert status == AbsorbStatus.FullyAbsorbed
 
-        data_sizes = [211, 148, 183]
+        data_sizes = [211, 149, 184]
         for i in range(100):
             # fmk.lg.export_raw_data = True
             data = fmk.process_data(
@@ -4186,8 +4186,6 @@ class TestDataModelHelpers(unittest.TestCase):
 
         assert i == 3
 
-        raise ValueError
-
         specific_cases_checked = False
         for i in range(100):
             data = fmk.process_data(
@@ -4205,7 +4203,7 @@ class TestDataModelHelpers(unittest.TestCase):
             raise ValueError
 
         # number of test cases
-        self.assertEqual(i, 22)
+        self.assertIn(i, [21,22])
         self.assertTrue(specific_cases_checked)
 
 class TestFMK(unittest.TestCase):