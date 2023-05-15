k0retux/fuddly

commit 0af56f3e0b6531542cc0c74fab3deeadc415b3d5

Plotty : fix wrong curve shifting

tools/plotty/plot/PlottyFigureArea.py
@@ -51,8 +51,8 @@ class PlottyFigureArea:
             return
 
         reference_first = reference_area.main_geometry.points[0]
-        first = self.__main_geometry.points[0]
-        last = self.__main_geometry.points[-1]
+        first = reference_area.main_geometry.points[0]
+        last = reference_area.main_geometry.points[-1]
         curve_height = abs(last.y - first.y)
         shift_x = reference_first.x - first.x
         shift_y = reference_first.y - first.y + \

commit 8b8f91ccea889ab2ee5b9c45b09301bb4202d849

Plotty : fix curve height bug introduced by 0af56f3

tools/plotty/plot/PlottyFigureArea.py
@@ -51,9 +51,9 @@ class PlottyFigureArea:
             return
 
         reference_first = reference_area.main_geometry.points[0]
-        first = reference_area.main_geometry.points[0]
-        last = reference_area.main_geometry.points[-1]
-        curve_height = abs(last.y - first.y)
+        reference_last = reference_area.main_geometry.points[-1]
+        first = self.__main_geometry.points[0]
+        curve_height = abs(reference_first.y - reference_last.y)
         shift_x = reference_first.x - first.x
         shift_y = reference_first.y - first.y + \
             self.__index * curve_height * \