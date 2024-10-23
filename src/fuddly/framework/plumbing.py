4d9014b1f79ae5e7b1a19f071aebeff372d089a0
Nicolai Dagestad <nicolai.github@dagestad.fr>
2024-10-23 13:57:02 +0200

    Update project detection logic
    Exclude __init__.py file in user_data_folder/user_projects/
    Add files and modules in current folder (not just the files as before)
    Change recursive call to a loop

--- src/fuddly/framework/plumbing.py
+++ src/fuddly/framework/plumbing.py
@@ -935,35 +935,50 @@ class FmkPlumbing(object):
         projects = collections.OrderedDict()
 
         def populate_projects(search_path, prefix=""):
-            prj_dir = os.path.basename(os.path.normpath(search_path))
-            (path, dirs, files) = next(os.walk(search_path))
-            if "__init__.py" in files:
-                # Normapath remove the / that will most likely be at the end of prefix
-                projects[os.path.normpath(prefix)] = [os.path.basename(search_path)]
-                return
-
-            for d in [x for x in dirs if x != "__pycache__"]:
-                # Done recursively manually even though walk does go down a depth of one folder
-                # but only one folder, so it would be nicer to do it ourselves
-                populate_projects(
-                    os.path.join(search_path, d),
-                    os.path.join(prefix, prj_dir) + os.sep
-                )
-
-            files = list(
-                    map(
-                        lambda x: x.removesuffix(".py"),
-                        filter(lambda x: x.endswith(".py"), files)
+            search_path=os.path.normpath(search_path)
+            for (path, dirs, files) in os.walk(search_path):
+                rel_path=path.removeprefix(search_path)
+                if "__init__.py" in files:
+                    # Normapath remove the / that will most likely be at the end of prefix
+                    #key=os.path.dirname(key)
+                    key=os.path.normpath(os.path.join(
+                            prefix,
+                            os.path.dirname(rel_path).removeprefix("/"),
+                        ))
+                    basename=os.path.basename(path.removeprefix(search_path).removeprefix("/"))
+                    print(basename)
+                    if basename != "":
+                        if projects.get(key) is None:
+                            projects[key] = []
+                        projects[key].append(basename)
+                        dirs.clear()
+                        continue
+                if "__pycache__" in dirs:
+                    dirs.remove("__pycache__")
+                # Take all python files except for __init__.py and remove the .py suffix
+                files = list(
+                        map(lambda x: x.removesuffix(".py"),
+                            filter(lambda x: x.endswith(".py"), 
+                               filter(lambda x: x != "__init__.py", files)
+                            )
+                        )
                     )
-                )
-            if len(files) != 0:
-                projects[prefix+prj_dir] = files
+                if len(files) != 0:
+                    key=os.path.normpath(os.path.join(
+                            prefix,
+                            rel_path.removeprefix("/"),
+                        ))
+                    if projects.get(key) is None:
+                        projects[key] = []
+                    projects[key].extend(files)
 
         if gr.is_running_from_fs:
             if not self._quiet:
                 self.print(colorize("*** Running directly from sources, loading internal projects ***", rgb=Color.WARNING))
-            populate_projects(gr.projects_folder, prefix="fuddly/")
-        populate_projects(gr.user_projects_folder)
+            populate_projects(gr.projects_folder, prefix="fuddly/projects")
+        populate_projects(gr.user_projects_folder, prefix="user_projects")
+
+        print(projects)
 
         for dname, file_list in projects.items():
             if not self._quiet:
@@ -1014,10 +1029,10 @@ class FmkPlumbing(object):
 
     def _import_project(self, prefix, name, reload_prj=False):
         try: 
-            if importlib.util.find_spec(prefix + name) is None:
-                name += "_proj"
+            #if importlib.util.find_spec(prefix + name) is None:
+            #    name += "_proj"
             module = importlib.import_module(prefix + name)
-            name = name.removesuffix("_proj")
+            #name = name.removesuffix("_proj")
 
             if reload_prj:
                 importlib.reload(module)
