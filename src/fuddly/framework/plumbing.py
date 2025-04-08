a692ca3b84642a403e51be904390d6833c9f8730
Eric Lacombe <eric.lacombe@security-labs.org>
2025-04-08 15:06:40 +0200

    Change @callback_func of FmkPlumbing.process_data_and_send() into @validator_func
    The semantics of the parameter has changed:
    "If not None, it will be called on each generated data. It shall return True,
    in order for the data to be sent and False otherwise."

--- src/fuddly/framework/plumbing.py
+++ src/fuddly/framework/plumbing.py
@@ -2426,7 +2426,7 @@ class FmkPlumbing(object):
                               max_loop=1, tg_ids=None,
                               verbose=False, console_display=True,
                               save_generator_seed=False,
-                              callback_func=None,
+                              validator_func=None,
                               reset_dmakers=False):
         """
         Send data to the selected targets. These data can follow a specific processing before
@@ -2449,7 +2449,8 @@ class FmkPlumbing(object):
             save_generator_seed: If random Generators are used, the generated data will be internally saved
               and will be reused next time this generator will be called, until
               FmkPlumbing.cleanup_dmaker(... reset_existing_seed=True) is called on this Generator.
-            callback_func: If not None, it will be called on each generated data.
+            validator_func: If not None, it will be called on each generated data. It shall return `True`
+              in order for the data to be sent and `False` otherwise.
             reset_dmakers: If `True`, the Generators and Operators will be reset before being used
 
         Returns:
@@ -2493,8 +2494,9 @@ class FmkPlumbing(object):
                     if data is None:
                         data = Data()
                         data.make_unusable()
-                    elif callback_func is not None:
-                        callback_func(data)
+                    elif validator_func is not None:
+                        if not validator_func(data):
+                            continue
 
                     if tg_ids:
                         data.tg_ids = tg_ids
@@ -2514,8 +2516,8 @@ class FmkPlumbing(object):
             sent_data = []
             while cpt < max_loop or max_loop == -1:
                 cpt += 1
-                if callback_func is not None:
-                    callback_func(data)
+                if validator_func is not None:
+                    validator_func(data)
                 go_on, sdata = self.send_data_and_log(data, verbose=verbose,
                                                       console_display=console_display)
                 if sdata:
