ae61ab2fa38c6ae54581cfb885803934f0dc5479
Nicolai Dagestad <nicolai.github@dagestad.fr>
2026-01-20 12:20:16 +0100

    Make generic module retrieval functions for projects/data_models/targets

--- src/fuddly/libs/fmk_services.py
+++ src/fuddly/libs/fmk_services.py
@@ -6,25 +6,68 @@ from importlib.metadata import entry_points
 import fuddly.framework.global_resources as gr
 from fuddly.framework.plumbing import _populate_projects as populate_projects
 
-def get_each_project_module() -> []:
 
-    project_modules = []
-
+# Get all modules (from FS and entry_points)
+def get_module_of_type(group_name: str, prefix: str):
+    path = {
+        "targets": gr.user_targets_folder,
+        "projects": gr.user_projects_folder,
+        "data_models": gr.user_data_models_folder,
+    }[group_name]
+    modules = []
     # Project from user (FS)
-    projects = populate_projects(gr.user_projects_folder, prefix="fuddly/projects", projects=None)
-    for dname, (_, file_list) in projects.items():
+    modules.extend(find_modules_in_dir(path=path, prefix=prefix))
+    # Projects from modules
+    modules.extend(find_modules_from_ep_group(group_name=gr.ep_group_names[group_name]))
+    return modules
+
+
+def get_each_project_module() -> list():
+    return get_module_of_type(
+            group_name="projects",
+            prefix="fuddly/projects"
+        )
+
+
+def get_each_data_model_module() -> list():
+    return get_module_of_type(
+            group_name="data_models",
+            prefix="fuddly/data_models"
+        )
+
+
+def get_each_target_module() -> list():
+    return get_module_of_type(
+            group_name="targets",
+            prefix="fuddly/data_models"
+        )
+
+
+# Find python modules in a specific path, prepend "prefix" to the modules' names
+def find_modules_in_dir(path: str, prefix: str) -> list():
+    res = []
+    fullpath = os.path.join(gr.fuddly_data_folder, path)
+    # TODO this is the project specific detector which checks if it's a python
+    # module. The data model import should be using the same behavior but has
+    # not been converted yet
+    modules = populate_projects(fullpath, prefix=prefix)
+    for dname, (_, file_list) in modules.items():
         prefix = dname.replace(os.sep, ".") + "."
         for name in file_list:
             m = find_spec(prefix+name)
             # This should never happen
             if m is None or m.origin is None:
-                print(f"{prefix+name} detected as a module in {gr.fuddly_data_folder}/user_projects,"
+                print(f"{prefix+name} detected as a module in "f"{fullpath},"
                       " but could not be imported")
                 continue
-            project_modules.append(m)
+            res.append(m)
+    return res
 
-    # Projects from modules
-    for ep in entry_points(group=gr.ep_group_names["projects"]):
+
+# Get all the python modules corresponding to a certain entry_point name group
+def find_modules_from_ep_group(group_name: str) -> list():
+    res = []
+    for ep in entry_points(group=group_name):
         if ep.name.endswith("__root__"):
             continue
         m = find_spec(ep.module)
@@ -34,9 +77,8 @@ def get_each_project_module() -> []:
             # the entry point is not a module, let's just ignore it
             print(f"*** {ep.module} is not a python module, check your installed modules ***")
             continue
-        project_modules.append(m)
-
-    return project_modules
+        res.append(m)
+    return res
 
 
 def get_project_from_name(name):
