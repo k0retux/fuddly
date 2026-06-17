89d8da0bac9ac3a6435385849268bfd7903a3225
Nicolai Dagestad <nicolai.github@dagestad.fr>
2026-06-17 10:12:48 +0200

    Only keep relevant parts of the argparse wrapper
    + Typo in docs

--- src/fuddly/cli/argparse_wrapper.py
+++ src/fuddly/cli/argparse_wrapper.py
@@ -1,4 +1,6 @@
 import argparse
+# This wildcard import makes this module contains everything from the original
+# argpars making it a drop-in replacement
 from argparse import *
 import os
 
@@ -25,57 +27,47 @@ contextualize(argparse.ArgumentParser)
 # Taken from https://mail.python.org/pipermail/stdlib-sig/2015-July/000990.html
 # Replaced string interpolation with f-strings and syntax tweak
 class PathType(object):
-    def __init__(self, exists=True, type='file', dash_ok=True):
+    def __init__(self, exists=True, type='file'):
         '''exists:
                 True: a path that does exist
                 False: a path that does not exist, in a valid parent directory
                 None: don't care
            type: file, dir, symlink, None, or a function returning True for valid paths
                 None: don't care
-           dash_ok: whether to allow "-" as stdin/stdout'''
+        '''
 
         assert exists in (True, False, None)
         assert type in ('file', 'dir', 'symlink', None) or hasattr(type, '__call__')
 
         self._exists = exists
         self._type = type
-        self._dash_ok = dash_ok
 
     def __call__(self, string):
-        if string == '-':
-            # the special argument "-" means sys.std{in,out}
-            if self._type == 'dir':
-                raise ArgumentTypeError('standard input/output (-) not allowed as directory path')
+        e = os.path.exists(string)
+        if self._exists is True:
+            if not e:
+                raise ArgumentTypeError(f"path does not exist: '{string}'")
+            if self._type is None:
+                pass
+            elif self._type == 'file':
+                if not os.path.isfile(string):
+                    raise ArgumentTypeError(f"path is not a file: '{string}'")
             elif self._type == 'symlink':
-                raise ArgumentTypeError('standard input/output (-) not allowed as symlink path')
-            elif not self._dash_ok:
-                raise ArgumentTypeError('standard input/output (-) not allowed')
+                if not os.path.islink(string):
+                    raise ArgumentTypeError(f"path is not a symlink: '{string}'")
+            elif self._type == 'dir':
+                if not os.path.isdir(string):
+                    raise ArgumentTypeError(f"path is not a directory: '{string}'")
+            elif not self._type(string):
+                raise ArgumentTypeError(f"path not valid: '{string}'")
         else:
-            e = os.path.exists(string)
-            if self._exists is True:
-                if not e:
-                    raise ArgumentTypeError(f"path does not exist: '{string}'")
-                if self._type is None:
-                    pass
-                elif self._type == 'file':
-                    if not os.path.isfile(string):
-                        raise ArgumentTypeError(f"path is not a file: '{string}'")
-                elif self._type == 'symlink':
-                    if not os.path.symlink(string):
-                        raise ArgumentTypeError(f"path is not a symlink: '{string}'")
-                elif self._type == 'dir':
-                    if not os.path.isdir(string):
-                        raise ArgumentTypeError(f"path is not a directory: '{string}'")
-                elif not self._type(string):
-                    raise ArgumentTypeError(f"path not valid: '{string}'")
-            else:
-                if self._exists is False and e:
-                    raise ArgumentTypeError(f"path exists: '{string}'")
+            if self._exists is False and e:
+                raise ArgumentTypeError(f"path exists: '{string}'")
 
-                p = os.path.dirname(os.path.normpath(string)) or '.'
-                if not os.path.isdir(p):
-                    raise ArgumentTypeError(f"parent path is not a directory: '{p}'")
-                elif not os.path.exists(p):
-                    raise ArgumentTypeError(f"parent directory does not exist: '{p}'")
+            p = os.path.dirname(os.path.normpath(string)) or '.'
+            if not os.path.isdir(p):
+                raise ArgumentTypeError(f"parent path is not a directory: '{p}'")
+            elif not os.path.exists(p):
+                raise ArgumentTypeError(f"parent directory does not exist: '{p}'")
 
         return string
